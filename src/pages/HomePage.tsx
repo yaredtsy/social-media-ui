a6281766f758d9843ef0d99ba21ce9d299362ef2
yared tsegaye <tsyared@gmail.com>
2022-08-20 21:50:32 +0300

    refa: grid refactored

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,13 +16,13 @@ const HomePage = () => {
           <MyProfile />
           <Following />
         </Col>
-        <Col xl={6} lg={8} md={12} className=" mb-6">
+        <Col xxl={6} xl={5} lg={8} md={12} className=" mb-6">
           <PostAction />
           <PostDisplay />
           <PostDisplay />
           <PostDisplay />
         </Col>
-        <Col xl={3} lg={3} className="disable-lg">
+        <Col xxl={3} xl={4} lg={3} className="disable-lg">
           <TopTranding />
         </Col>
       </Row>
